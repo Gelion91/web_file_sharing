03fabdc0354042f0fab9930aeb3022694674208a
Roman <gelion91@mail.ru>
2020-07-08 21:55:50 +0300

    Добавлены download and upload

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 import os
-from flask import Blueprint, render_template, flash, url_for, request
+from glob import glob
+
+from flask import Blueprint, render_template, flash, url_for, request, send_file
 from werkzeug.utils import redirect, secure_filename
-from main.forms import UploadForm
+from main.forms import UploadForm, DownloadForm, DeleteForm
 
 blueprint = Blueprint('main', __name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -22,7 +24,7 @@ def upload():
 
 @blueprint.route('/process_upload', methods=['POST'])
 def process_upload():
-    """ Добавление файла """
+    """ Добавить файл """
     upload_form = UploadForm()
     if upload_form.validate_on_submit():
         if request.method == 'POST':
@@ -31,7 +33,8 @@ def process_upload():
             filename = str(hash(filename))
             if not os.path.exists(os.path.join(basedir, '..', 'store')):
                 os.mkdir(os.path.join(basedir, '..', 'store'))
-            os.mkdir(os.path.join(basedir, '..', 'store', filename[:2]))
+            if not os.path.exists(os.path.join(basedir, '..', 'store', filename[:2])):
+                os.mkdir(os.path.join(basedir, '..', 'store', filename[:2]))
             f.save(os.path.join(basedir, '..', 'store', filename[:2], filename))
 
         flash('Файл сохранен')
@@ -45,8 +48,48 @@ def process_upload():
 @blueprint.route('/download')
 def download():
     title = 'Скачать'
+    download_form = DownloadForm()
+    return render_template('download/download.html', page_title=title, form=download_form)
+
+
+@blueprint.route('/process_download', methods=['GET', 'POST'])
+def process_download():
+    """ Скачать файл """
+    download_form = DownloadForm()
+    if download_form.validate_on_submit():
+        if request.method == 'POST':
+            result = download_form.file.data
+            try:
+                return send_file(os.path.join(basedir, '..', 'store', result[:2], result), as_attachment=True)
+            except FileNotFoundError:
+                flash('Файл не найден.')
+                return redirect(url_for('main.download'))
+
+    flash('Введите ключ.')
+    return redirect(url_for('main.download'))
 
 
 @blueprint.route('/delete')
 def delete():
     title = 'Удалить'
+    delete_form = DeleteForm()
+    return render_template('delete/delete.html', page_title=title, form=delete_form)
+
+
+@blueprint.route('/process_delete', methods=['GET', 'POST'])
+def process_delete():
+    """ Удаление файла """
+    delete_form = DeleteForm()
+    if delete_form.validate_on_submit():
+        if request.method == 'POST':
+            result = delete_form.file.data
+            try:
+                os.remove(os.path.join(basedir, '..', 'store', result[:2], result))
+                flash('Файл успешно удален.')
+                return redirect(url_for('main.delete'))
+            except FileNotFoundError:
+                flash('Файл не найден.')
+                return redirect(url_for('main.delete'))
+
+    flash('Введите ключ.')
+    return redirect(url_for('main.delete'))
